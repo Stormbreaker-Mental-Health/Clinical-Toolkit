d8133eebfca42ae5f1b1acb4eeea878d487f8a5e
DaveAldon <DaveAldon@users.noreply.github.com>
2024-06-03 19:56:59 +0000

    Deploying to public from @ Stormbreaker-Mental-Health/Clinical-Toolkit@dce739e90e76619ccf100bd8856ce2f945219919 🚀

---
+++ _next/static/2XWhx2gSGASFBd_3rxmVs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-3084a3af29bd6e3f.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
